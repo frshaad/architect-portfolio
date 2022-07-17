ff8a836ba9c4fcd8ee256b992767adc1673d1fea
Frshaad <farshad.hatami95@gmail.com>
2022-07-17 14:54:45 +0430

    add work style

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,44 @@ const scrollHeader = () => {
 		: header.classList.remove('scroll-header');
 };
 window.addEventListener('scroll', scrollHeader);
+
+// **************
+// Services Modal
+const modalViews = document.querySelectorAll('.services__modal');
+const modalBtns = document.querySelectorAll('.services__button');
+const modalClose = document.querySelectorAll('.services__modal-close');
+
+let modal = function (modalClick) {
+	modalViews[modalClick].classList.add('active-modal');
+};
+
+modalBtns.forEach((modalBtn, i) => {
+	modalBtn.addEventListener('click', () => {
+		modal(i);
+	});
+});
+
+modalClose.forEach(modalClose => {
+	modalClose.addEventListener('click', () => {
+		modalViews.forEach(modalView => {
+			modalView.classList.remove('active-modal');
+		});
+	});
+});
+// Services Modal
+// **************
+
+// **************
+// Work MixItUp
+
+let mixerPortfolio = mixitup('.work__container', {
+	selectors: {
+		target: '.work__card',
+	},
+	animation: {
+		duration: 300,
+	},
+});
+
+// Work MixItUp
+// **************
