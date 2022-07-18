d4dcbb2d950e87dd76fc0f80bb7504e0327cc98c
Frshaad <farshad.hatami95@gmail.com>
2022-07-18 15:47:32 +0430

    Mobile version done

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
 const scrollHeader = () => {
 	const header = document.getElementById('header');
-	// When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
 	this.scrollY >= 50
 		? header.classList.add('scroll-header')
 		: header.classList.remove('scroll-header');
@@ -49,17 +48,17 @@ let mixerPortfolio = mixitup('.work__container', {
 const linkWork = document.querySelectorAll('.work__item');
 
 function activeWork() {
-	linkWork.forEach(link => link.classList.remove('active-work'));
+	linkWork.forEach(l => l.classList.remove('active-work'));
 	this.classList.add('active-work');
 }
 
-linkWork.forEach(link => link.addEventListener('click', activeWork));
+linkWork.forEach(l => l.addEventListener('click', activeWork));
 
-// Work MixItUp
-// **************
+// // Work MixItUp
+// // **************
 
-// **************
-// Swiper Testimonial
+// // **************
+// // Swiper Testimonial
 
 let swiperTestimonial = new Swiper('.testimonial__container', {
 	spaceBetween: 24,
@@ -80,5 +79,56 @@ let swiperTestimonial = new Swiper('.testimonial__container', {
 	},
 });
 
-// Swiper Testimonial
+// // Swiper Testimonial
+// // **************
+
+// // **************
+// // Scroll Sections Active Link
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+	const scrollY = window.pageYOffset;
+
+	sections.forEach(current => {
+		const sectionHeight = current.offsetHeight,
+			sectionTop = current.offsetTop - 58,
+			sectionId = current.getAttribute('id'),
+			sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+			sectionsClass.classList.add('active-link');
+		} else {
+			sectionsClass.classList.remove('active-link');
+		}
+	});
+};
+window.addEventListener('scroll', scrollActive);
+
+// Scroll Sections Active Link
 // **************
+
+// **************
+// Theme Change
+const themeButton = document.getElementById('theme-button');
+const lightTheme = 'light-theme';
+const iconTheme = 'bx-sun';
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => (document.body.classList.contains(lightTheme) ? 'dark' : 'light');
+const getCurrentIcon = () =>
+	themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun';
+
+if (selectedTheme) {
+	document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](lightTheme);
+	themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme);
+}
+
+themeButton.addEventListener('click', () => {
+	document.body.classList.toggle(lightTheme);
+	themeButton.classList.toggle(iconTheme);
+	localStorage.setItem('selected-theme', getCurrentTheme());
+	localStorage.setItem('selected-icon', getCurrentIcon());
+});
