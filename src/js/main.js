1dd5403724adb8593c59de5087bc99470d845d48
frshaad <farshad.hatami95@gmail.com>
2022-07-22 11:37:55 +0430

    Desktop styles added

--- src/js/main.js
+++ src/js/main.js
@@ -73,7 +73,7 @@ let swiperTestimonial = new Swiper('.testimonial__container', {
 			slidesPerView: 2,
 		},
 		768: {
-			slidesPerView: 2,
+			slidesPerView: 3,
 			spaceBetween: 48,
 		},
 	},
@@ -106,29 +106,3 @@ window.addEventListener('scroll', scrollActive);
 
 // Scroll Sections Active Link
 // **************
-
-// **************
-// Theme Change
-const themeButton = document.getElementById('theme-button');
-const lightTheme = 'light-theme';
-const iconTheme = 'bx-sun';
-
-// Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme');
-const selectedIcon = localStorage.getItem('selected-icon');
-
-const getCurrentTheme = () => (document.body.classList.contains(lightTheme) ? 'dark' : 'light');
-const getCurrentIcon = () =>
-	themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun';
-
-if (selectedTheme) {
-	document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](lightTheme);
-	themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme);
-}
-
-themeButton.addEventListener('click', () => {
-	document.body.classList.toggle(lightTheme);
-	themeButton.classList.toggle(iconTheme);
-	localStorage.setItem('selected-theme', getCurrentTheme());
-	localStorage.setItem('selected-icon', getCurrentIcon());
-});
